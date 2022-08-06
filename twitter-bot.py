12012fa8f5131c325b3c9106543fe957b6402396
Kevin Volmhagen <43371558+KevinOW@users.noreply.github.com>
2022-08-06 03:38:46 +0200

    Update twitter-bot.py

--- twitter-bot.py
+++ twitter-bot.py
@@ -1,11 +1,6 @@
 import tweepy
 import logging
 
-#consumer_key = 'fTx4UnNpno2qXW0TiuPw72mgW'
-#consumer_secret = 'EEEGl8Rrq4z0GAAJvXUv1dCPfeMh35GimTDuSX6hjTkNCulgoO'
-#key = '1490181790014836737-IJqqU5twh0tVtqqNBnrt63nj8NNfA'
-#secret = 'oHMfkfx2kYGPtYUdNwS7n64gGFCXIPGHyOREfKgLKM0in'
-
 logging.basicConfig(filename='backlog.log', level=logging.DEBUG,
                     format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
